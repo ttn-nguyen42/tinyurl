631aef4b805c43c4c47c21a9cbea0adfe732dc4f
Nguyen Tran <nguyentt1@vnpay.vn>
2024-03-01 15:58:39 +0700

    updated redirect log

--- src/main/java/org/ntranlab/url/routes/redirect/RedirectController.java
+++ src/main/java/org/ntranlab/url/routes/redirect/RedirectController.java
@@ -3,6 +3,8 @@ package org.ntranlab.url.routes.redirect;
 import org.ntranlab.url.business.routers.RouterService;
 import org.ntranlab.url.models.redirect.RedirectRequest;
 import org.ntranlab.url.models.redirect.RedirectResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +14,8 @@ import org.springframework.web.servlet.view.RedirectView;
 @RestController
 public class RedirectController {
     private final RouterService routerService;
+
+    private Logger logger = LoggerFactory.getLogger(RedirectController.class);
 
     public RedirectController(RouterService routerService) {
         this.routerService = routerService;
@@ -25,9 +29,24 @@ public class RedirectController {
      */
     @RequestMapping(value = "/to/{siteId}", method = RequestMethod.GET)
     public RedirectView redirect(@PathVariable("siteId") String siteId) {
-        RedirectResult result = this.routerService.getRedirection(RedirectRequest.builder()
-                .alias(siteId)
-                .build());
-        return new RedirectView(result.getDestination());
+        try {
+            RedirectResult result = this.routerService.getRedirection(RedirectRequest.builder()
+                    .alias(siteId)
+                    .build());
+            String destination = result.getDestination();
+            logger.info("RedirectController.redirect: siteId = "
+                    + siteId
+                    + ", to = "
+                    + destination);
+
+            return new RedirectView(destination);
+        } catch (Exception e) {
+            logger.warn("RedirectController.redirect: siteId = "
+                    + siteId
+                    + ", error = "
+                    + e.getMessage());
+            throw e;
+        }
+
     }
 }
